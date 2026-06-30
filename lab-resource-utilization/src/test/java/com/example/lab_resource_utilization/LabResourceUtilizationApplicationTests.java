046cdbbb37d5f25c67b3e343e8eb1f15d160a1b6
priyadharshinimanikandan25-art <priyadharshinimanikandan25@gmail.com>
2026-06-30 22:32:36 +0530

    Initial Spring Boot project setup

---
+++ lab-resource-utilization/src/test/java/com/example/lab_resource_utilization/LabResourceUtilizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_resource_utilization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResourceUtilizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
